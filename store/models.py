ff0cd925c60ac757f62c0431e5fb09efde9cbd68
Vadim Kalashnikov <vadkal@live.spbstu.ru>
2024-02-09 17:10:56 +0300

    Added Wishlist3

--- store/models.py
+++ store/models.py
@@ -70,7 +70,7 @@ class Cart(models.Model):
 
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=1)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
